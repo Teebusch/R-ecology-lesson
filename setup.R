7c107b50610a6c180cf3a204b1ccd87684872c89
Tobias Busch <teebusch@gmail.com>
2020-10-28 23:53:00 +0100

    change to distill, update first episodes

--- setup.R
+++ setup.R
@@ -28,33 +28,31 @@ if (!file.exists("data_raw/portal_mammals.sqlite")) {
 library(knitr)
 library(methods)
 suppressPackageStartupMessages(library(tidyverse))
-knitr::opts_chunk$set(results='hide', fig.path='img/R-ecology-',
-                      comment = "#>", purl = FALSE, fig.keep='last')
+knitr::opts_chunk$set(
+    results   = 'hide',
+    echo      = TRUE,   # default in Distill is FALSE
+    purl      = FALSE,
+    comment   = "#>",
+    fig.path  = 'img/R-ecology-',
+    fig.keep  = 'last'
+)
 
-### Custom hooks
 
-## hook for challenges answers
+## Custom hooks
+
+### hook for challenges answers
 
 knitr::knit_hooks$set(answer = function(before, options, envir) {
     if (before) {
-        paste(
-            "<div class=\"accordion\">",
-              "<h3 class=\"toc-ignore\">Answer</h3>",
-              "<div style=\"background: #fff;\">",  sep = "\n")
+        "<details>\n<summary>Answer</summary>\n<p>"
     } else {
-        paste("</div>", "</div>", sep = "\n")
+        "</p>\n</details>"
     }
 })
 
 eng_text_answer <- knitr:::eng_html_asset(
-                               paste(
-                                   "<div class=\"accordion\">",
-                                   "<h3 class=\"toc-ignore\">Answer</h3>",
-                                   "<div style=\"background: #fff;\">",
-                                   "<p>",  sep = "\n"),
-                               paste(
-                                   "</div>", "</div>", "</p>", sep = "\n"
-                               )
-                           )
+    "<details>\n<summary>Answer</summary>\n<p>",
+    "</p>\n</details>"
+)
 
 knitr::knit_engines$set(text_answer = eng_text_answer)
